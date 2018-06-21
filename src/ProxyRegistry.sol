c4775aabe775af102bfbe5e8d496e1982a76f3db
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2018-06-21 10:39:37 -0300

    Upgrade packages + approval system for creating a third party proxy

--- src/ProxyRegistry.sol
+++ src/ProxyRegistry.sol
@@ -1,16 +1,27 @@
 pragma solidity ^0.4.18;
 
 import 'ds-proxy/proxy.sol';
+import 'ds-auth/auth.sol';
+
 
 // ProxyRegistry
 // This Registry deploys new proxy instances through DSProxyFactory.build(address) and keeps a registry of owner => proxies
-contract ProxyRegistry {
+contract ProxyRegistry is DSAuth {
     mapping(address=>DSProxy[]) public proxies;
     mapping(address=>uint) public proxiesCount;
+    mapping(address=>bool) public approvals;
     DSProxyFactory factory;
 
-    function ProxyRegistry(DSProxyFactory factory_) public {
+    constructor(DSProxyFactory factory_) public {
         factory = factory_;
+    }
+
+    function rely(address guy) public auth {
+        approvals[guy] = true;
+    }
+
+    function deny(address guy) public auth {
+        approvals[guy] = false;
     }
 
     // deploys a new proxy instance
@@ -22,6 +33,7 @@ contract ProxyRegistry {
     // deploys a new proxy instance
     // sets custom owner of proxy
     function build(address owner) public returns (DSProxy proxy) {
+        require(msg.sender == owner || approvals[msg.sender]);
         proxy = factory.build(owner);
         proxies[owner].push(proxy);
         proxiesCount[owner] ++;
--- src/ProxyRegistry.t.sol
+++ src/ProxyRegistry.t.sol
@@ -4,13 +4,25 @@ import "ds-test/test.sol";
 
 import "./ProxyRegistry.sol";
 
+contract FakeUser {
+	function doBuild(ProxyRegistry registry, address owner) public returns (DSProxy proxy) {
+		proxy = registry.build(owner);
+	}
+
+	function doRely(ProxyRegistry registry, address guy) public {
+		registry.rely(guy);
+	}
+}
+
 contract ProxyRegistryTest is DSTest {
     ProxyRegistry registry;
     DSProxyFactory factory;
+	FakeUser user;
 
     function setUp() public {
         factory = new DSProxyFactory();
         registry = new ProxyRegistry(factory);
+		user = new FakeUser();
     }
 
 	function test_ProxyRegistryBuild() public {
@@ -37,7 +49,8 @@ contract ProxyRegistryTest is DSTest {
 
     function test_ProxyRegistryBuildOtherOwner() public {
 		address owner = address(0x123);
-		address proxyAddr = registry.build(owner);
+		registry.rely(user);
+		address proxyAddr = user.doBuild(registry, owner);
 		assertTrue(proxyAddr > 0x0);
 		DSProxy proxy = DSProxy(proxyAddr);
 
@@ -56,5 +69,34 @@ contract ProxyRegistryTest is DSTest {
 
         assertEq(registry.proxiesCount(owner), 1);
         assertEq(registry.proxies(owner, 0), proxy);
+	}
+
+	function test_ProxyRegistryRely() public {
+		registry.setOwner(user);
+		user.doRely(registry, this);
+		address owner = address(0x123);
+		address proxyAddr = registry.build(owner);
+		assertTrue(proxyAddr > 0x0);
+	}
+
+	function testFail_ProxyRegistryBuildOtherOwner() public {
+		address owner = address(0x123);
+		registry.build(owner);
+	}
+
+	function testFail_ProxyRegistryBuildOtherOwner2() public {
+		address owner = address(0x123);
+		user.doBuild(registry, owner);
+	}
+
+	function testFail_ProxyRegistryBuildOtherOwner3() public {
+		address owner = address(0x123);
+		registry.rely(user);
+		registry.deny(user);
+		user.doBuild(registry, owner);
+	}
+
+	function testFail_ProxyRegistryRely() public {
+		user.doRely(registry, address(0x123));
 	}
 }
